245897ccd657892ae37cf863e0e1cd76b4883b58
Simon Woolf <simon@simonwoolf.net>
2014-04-22 14:49:44 +0100

    formatting

--- markov.rb
+++ markov.rb
@@ -5,7 +5,9 @@ def file_to_string(filename)
 end
 
 def doubles(filename)
-  file_to_string(filename).scrub.split.each_cons(2)
+  file_to_string(filename).scrub
+                          .split
+                          .each_cons(2)
 end
 
 def generate_cache(filename)
@@ -25,7 +27,9 @@ def successor_count(word, cache)
 end
 
 def popular_successors_to(word, cache, n=10)
-  successor_count(word, cache).sort_by{|w, count| count}.reverse.first(n)
+  successor_count(word, cache).sort_by{|w, count| count}
+                              .reverse
+                              .first(n)
 end
 
 cache = generate_cache("inputs/foundation.txt")
