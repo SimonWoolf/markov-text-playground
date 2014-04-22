c64ada026fd931546e21887c8f7c49a576351353
Simon Woolf <simon@simonwoolf.net>
2014-04-22 14:46:19 +0100

    successor count

--- markov.rb
+++ markov.rb
@@ -9,13 +9,22 @@ def doubles(filename)
 end
 
 def generate_cache(filename)
+  # cache is a hash with values an array of all words occuring 
+  # immediately after the key
   doubles(filename).each_with_object({}) do |(first, second), cache|
     cache[first] ||= []
     cache[first] << second
   end
 end
 
+def successor_count(word, cache)
+  cache[word].each_with_object({}) do |successor, counts|
+    counts[successor] ||= 1
+    counts[successor] += 1
+  end
+end
+
+cache = generate_cache("inputs/foundation.txt")
+
 require 'awesome_print'
-ap generate_cache("inputs/foundation.txt")['outer']
-
-
+ap successor_count("the", cache).sort_by{|word, count| count}.reverse.first(10)
