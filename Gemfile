817db9ee7988737c598c10d10dd75ea1242058ae
Simon Woolf <simon@simonwoolf.net>
2014-05-21 13:27:54 +0100

    sinatra initial setup

--- Gemfile
+++ Gemfile
@@ -2,9 +2,11 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "thin"
+gem "haml"
 
 group :development do
   gem "shotgun"
+  gem "better_errors"
 end
 
 group :test do
--- server.rb
+++ server.rb
@@ -1,2 +1,20 @@
 require 'sinatra'
 require 'haml'
+require 'better_errors'
+
+# Settings
+set :partial_template_engine, :haml
+configure :development do
+  use BetterErrors::Middleware
+  BetterErrors.application_root = File.expand_path('.', __FILE__)
+end
+
+
+# Controllers
+get '/' do
+  "#{ENV['RACK_ENV']}"
+end
+
+# Helpers
+helpers do
+end
